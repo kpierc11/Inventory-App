499b980c44c4040f1524ada1c17708a44aec4adc
Emberwind-Studios <kalebpierce@gmail.com>
2026-08-11 20:51:37 -0400

    Added basic subscriber functionality

--- store/config/routes.rb
+++ store/config/routes.rb
@@ -16,13 +16,16 @@ Rails.application.routes.draw do
   root "products#index"
   resources :products
 
-#  get "/products", to: "products#index"
-#  get "/products/new", to: "products#new"
-#  post "/products", to: "products#create"
-#  get "/products/:id", to: "products#show"
-#  get "/products/:id/edit", to: "products#edit"
-#  patch "/products/:id", to: "products#update"
-#  put "/products/:id", to: "products#update"
-#  delete "/products/:id", to: "products#destroy"
+   resources :products do
+    resources :subscribers, only: [ :create ]
+   end
 
+  #  get "/products", to: "products#index"
+  #  get "/products/new", to: "products#new"
+  #  post "/products", to: "products#create"
+  #  get "/products/:id", to: "products#show"
+  #  get "/products/:id/edit", to: "products#edit"
+  #  patch "/products/:id", to: "products#update"
+  #  put "/products/:id", to: "products#update"
+  #  delete "/products/:id", to: "products#destroy"
 end
